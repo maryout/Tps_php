6a3f4e36cc0c4213fb18433efd2088e5ab234575
taoufiqcheniguer <119008958+taoufiqcheniguer@users.noreply.github.com>
2022-11-29 01:07:52 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@
 //1 connexion à la base
 $serveur="localhost";
 $login="root";
-$password="";
+$password="123456";
 
   $con = new PDO('mysql:host=localhost;dbname=grh', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
   if($con==false) echo "pb";
